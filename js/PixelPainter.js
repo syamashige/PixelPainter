f642134606f23d04c6f6000fe67766b59c03a50f
Sarah <yamashige.sarah@gmail.com>
2018-07-09 20:24:38 -1000

    Eraser and Clear button working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,71 +1,105 @@
-var palette = [
+
+const palette = [
     '#001308', '#001308', '#1e272c', '#0b1b1a', '#17241e', '#450c41', '#61275d', '#86b5d5', '#2c7f84', '#224a72', '#83b5bd', '#b9e8ec', '#fdfce6', '#f0ddbf', '#d88282', '#ce9e55', '#ffdcb0', '#b5ccba', '#8ab25b', '#256a74',
     '#e8683c', '#f2c078', '#faedca', '#c1dbb3', '#89d8be', 
-    '#e7d1e0', '#f9def0', '#fbe8f6', '#f43433', 'f4eef4'
+    '#e7d1e0', '#f9def0', '#fbe8f6', '#f43433', '#f4eef4'
 ]
+//Add paintbrush cursor to the body 
+// document.body.style.cursor = "url('/assets/paintbrush_icon.png'), auto"
+
+let paletteCollector = null;
 
 // Container for the color swatches 
 var colorPalette = document.createElement('div');
-colorPalette.id = 'palette';
+colorPalette.id = 'colors';
 document.body.appendChild(colorPalette);
 
-// Creating boxes for the swatches 
-// Need to find a way to add in as many as it needs
-// for (var i = 0; i < 55; i++) {
-//     var paletteSwatches = document.createElement('div');
-//     paletteSwatches.className = 'swatches';
-//     colorPalette.appendChild(paletteSwatches);
-// }
 
 //Trying something else: 
-for (var i = 0; i < palette.length; i++) {
-    var paletteSwatches = document.createElement('div');
+for (let i = 0; i < palette.length ; i++) {
+    let paletteSwatches = document.createElement('div');
     paletteSwatches.className = 'swatches';
+    // paletteSwatches.innerHTML = palette[i];
     paletteSwatches.style.backgroundColor = palette[i];
     colorPalette.appendChild(paletteSwatches);
-    // console.log(palette[i]);
-
-    // var swatchPicker = document.getElementsByClassName('swatches');
-    // for (var j = 0; j < swatchPicker.length; j++) {
-    //     swatchPicker[j].addEventListener('click', grabColorSwatch);
-    // }
-    // function grabColorSwatch (event) {
-    //     var grabColor = this.querySelectorAll('.palette')[0];
-    //     console.log(grabColor);
-    // }
+    paletteSwatches.addEventListener('click', grabColorSwatch);
 }
 
-//Eraser Button
-var eraserButton = document.createElement('button');
-eraserButton.id = 'eraser';
-eraserButton.innerHTML = 'Eraser';
-colorPalette.appendChild(eraserButton);
+//Grab the color and store it in the Palette Collector
+let swatchPicker = document.getElementsByClassName('swatches');
+for (let j = 0; j < swatchPicker.length; j++) {
+    swatchPicker[j].addEventListener('click', grabColorSwatch);
+}
+function grabColorSwatch() {
+    console.log('pick color');
+    // console.log(palette[i]);
+    // this.style.backgroundColor = "white"; //testing
+    document.body.style.cursor = "url('/assets/paintbrush_icon.png'), auto";
+    paletteCollector = this.style.backgroundColor;
+    console.log(paletteCollector);
+}
 
-//Clear Button
-var clearButton = document.createElement('button');
-clearButton.id = 'clear';
-clearButton.innerHTML = 'Clear';
-colorPalette.appendChild(clearButton);
 
-
-// Container for the canvas pixels
-var paintCanvas = document.createElement('div');
+//Container for the canvas pixels
+let paintCanvas = document.createElement('div');
 paintCanvas.id = 'canvas';
 document.body.appendChild(paintCanvas);
 
-for (row = 0; row <= 10; row++) {
-    var rowPixels = document.createElement('div');
-    rowPixels.className = 'row';
-    paintCanvas.appendChild(rowPixels);
+//Create the pixel boxes
+const pixelBoxes = 132;
+for (let j = 0; j < pixelBoxes; j++) {
+    let thePixels = document.createElement('div');
+    thePixels.className = 'pixels';
+    paintCanvas.appendChild(thePixels);
+}
 
-    for (columns = 0; columns <= 10; columns++){
-        var columnPixels = document.createElement('div');
-        columnPixels.className = 'column';
-        paintCanvas.appendChild(columnPixels);
-    }
+//Add color to the pixel
+let pixelListener = document.getElementsByClassName('pixels');
+for (let j = 0; j < pixelListener.length; j++) {
+    pixelListener[j].addEventListener ('click', addColor);
+}
+
+//Moved the pixel testing listener outside of the for loop 
+function addColor () {
+    console.log('add color');
+    this.style.backgroundColor = paletteCollector;
+}
+
+//Eraser Button
+let eraserButton = document.createElement('button');
+eraserButton.id = 'eraser';
+eraserButton.innerHTML = 'Eraser';
+colorPalette.appendChild(eraserButton);
+eraserButton.addEventListener('click', eraseColor);
+
+function eraseColor() {
+    console.log('eraser is go')
+    paletteCollector = null;
+}
+document.getElementById('eraser').addEventListener('click', showEraserIcon); 
+
+//Change the cursor to an eraser on click
+function showEraserIcon() {
+    console.log('eraser cursor test');
+    document.body.style.cursor = "url('/assets/eraser_icon.png'), auto";  
 }
 
 
+//Clear Button
+let clearButton = document.createElement('button');
+clearButton.id = 'clear';
+clearButton.innerHTML = 'Clear';
+colorPalette.appendChild(clearButton);
+clearButton.addEventListener('click', clearCanvas);
+
+//Function to clear the canvas
+function clearCanvas() {
+    console.log('clear is a go');
+    let clearingPixels = document.getElementsByClassName('pixels');
+    for (let i = 0; i < clearingPixels.length; i++) {
+        clearingPixels[i].style.backgroundColor = null;
+    }
+}
 
 //Use this to get the dimensions of a certain box/div
 //console.log(box.getBoundingClientRect()); 
